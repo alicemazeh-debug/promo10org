d0816c999ca17e0455b013b7dd0f0ee874f1dcdc
Alice Mazeh <Alice.Mazeh@EITECH.LOCAL>
2026-06-23 11:10:16 +0300

    fix:Alice mazeh cv uploaded

---
+++ force-app/main/default/lwc/alicecv/alicecv.js
@@ -0,0 +1,7 @@
+import { LightningElement } from 'lwc';
+
+export default class Alicecv extends LightningElement {
+    name = 'Alice';
+    number= +961-81949591;
+    email = 'alice.mazeh@eit-mena.com';
+}
